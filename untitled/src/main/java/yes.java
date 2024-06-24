f412b4eb764a2a7b03b688f323ecc5e01d79dca1
PC <niknamed300@mail.ru>
2024-06-24 22:12:27 +0300

    Branch: Lesson_14_testng

---
+++ untitled/src/main/java/yes.java
@@ -0,0 +1,10 @@
+
+public class yes {
+    public int calculateFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
